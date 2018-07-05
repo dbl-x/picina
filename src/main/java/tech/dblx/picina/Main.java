e17a7604dbf3a6001ba23b31d154aaf26881e28f
linxin <dblx.lin@gmail.com>
2018-07-05 10:44:22 +0800

    init project

---
+++ src/main/java/tech/dblx/picina/Main.java
@@ -0,0 +1,11 @@
+package tech.dblx.picina;
+
+/**
+ * @author linxin (dblx.lin@gmail.com) 2018-07-05 10:41
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/test/java/tech/dblx/picina/Test.java
@@ -0,0 +1,7 @@
+package tech.dblx.picina;
+
+/**
+ * @author linxin (dblx.lin@gmail.com) 2018-07-05 10:40
+ */
+public class Test {
+}
